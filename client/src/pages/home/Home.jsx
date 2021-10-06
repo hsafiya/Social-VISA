ec380ac0fc15814634c0a367fc67b7a1379da262
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-10-06 18:15:42 -0500

    componenets

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
 
 export default function Home() {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />;
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
