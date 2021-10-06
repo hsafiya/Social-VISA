07ba45c6855b1a548ee668377473e16ae582b7b7
Victor Lupascu <sinajeen@gmail.com>
2021-10-05 22:39:16 -0500

    add user update route

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,46 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
-// signup
+// SIGNUP
 router.post('/signup', async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  })
-
   try {
+    //   hashing password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPsw = await bcrypt.hash(req.body.password, salt)
+
+    // create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPsw,
+    })
+
+    // return new user
     const user = await newUser.save()
     res.status(200).json(user)
   } catch (err) {
-    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    //   find user by email
+    const user = await User.findOne({
+      email: req.body.email,
+    })
+    !user && res.status(404).json('user not found')
+
+    // compare the passwords
+    const validPsw = await bcrypt.compare(req.body.password, user.password)
+    !validPsw && res.status(400).json('wrong password')
+
+    // return user
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
   }
 })
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,41 @@
 const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
-router.get('/', (req, res) => {
-  res.send('hello')
+// update user
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    //   if changing password
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10)
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+      } catch (err) {
+        return res.status(500).json(err)
+      }
+    }
+
+    // update anything else
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true },
+      )
+      res.status(200).json('Acc has been updated')
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(403).json('You can update only your acc!')
+  }
 })
+// delete user
+
+// get a user
+
+// follow a user
+
+// unfollow a user
 
 module.exports = router
