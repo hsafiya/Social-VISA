0bc15a8e25f95c5a166b47f1e1f2a3855ef97550
Victor Lupascu <sinajeen@gmail.com>
2021-10-06 00:54:38 -0500

    add posts timeline(needs refactoring)

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
+const User = require('../models/User')
 
-// create a post
+//create a post
+
 router.post('/', async (req, res) => {
   const newPost = new Post(req.body)
   try {
@@ -11,54 +13,54 @@ router.post('/', async (req, res) => {
     res.status(500).json(err)
   }
 })
+//update a post
 
-// update a post
 router.put('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
     if (post.userId === req.body.userId) {
       await post.updateOne({ $set: req.body })
-      res.status(200).json('post updated')
+      res.status(200).json('the post has been updated')
     } else {
-      res.status(403).json('update only your posts')
+      res.status(403).json('you can update only your post')
     }
   } catch (err) {
     res.status(500).json(err)
   }
 })
+//delete a post
 
-// delete a post
 router.delete('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
     if (post.userId === req.body.userId) {
       await post.deleteOne()
-      res.status(200).json('post deleted')
+      res.status(200).json('the post has been deleted')
     } else {
-      res.status(403).json('delete only your posts')
+      res.status(403).json('you can delete only your post')
     }
   } catch (err) {
     res.status(500).json(err)
   }
 })
+//like / dislike a post
 
-// like a post
 router.put('/:id/like', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } })
-      res.status(200).json('post has been liked')
+      res.status(200).json('The post has been liked')
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } })
-      res.status(200).json('post has been disliked')
+      res.status(200).json('The post has been disliked')
     }
   } catch (err) {
     res.status(500).json(err)
   }
 })
+//get a post
 
-// get a post
 router.get('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
@@ -68,6 +70,21 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-// get timeline posts
+//get timeline posts
+
+router.get('/timeline/all', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId)
+    const userPosts = await Post.find({ userId: currentUser._id })
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId })
+      }),
+    )
+    res.json(userPosts.concat(...friendPosts))
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router
