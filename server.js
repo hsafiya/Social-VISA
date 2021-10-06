7557e6b4b9d3efdb057d71c201b4874d6c015507
Victor Lupascu <sinajeen@gmail.com>
2021-10-05 20:31:05 -0500

    add sample routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const db = require('./config/connection')
 const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const userRoutes = require('./routes/users')
+const authRoutes = require('./routes/auth')
 
 // initiate dotenv
 dotenv.config()
@@ -16,6 +18,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
+
+// routes
+app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
 
 // connect to database
 db.once('open', () => {
