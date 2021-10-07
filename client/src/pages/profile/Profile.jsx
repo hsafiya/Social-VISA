7bbeeb0bec43573a40b258d83d93382c1933dd7b
Iryna <iryna.shulim@gmail.com>
2021-10-07 08:27:18 -0500

    profile jsx imports

---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,5 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
