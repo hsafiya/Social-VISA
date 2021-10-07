78eae1d8b052784bf6a0b7fce77c190489f0a83b
Iryna <iryna.shulim@gmail.com>
2021-10-07 10:27:45 -0500

    all imports to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 
 function App() {
   return <Profile/>;
