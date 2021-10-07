aae0dda64e323d978042a834bb81dda4ab7b66bd
Iryna <iryna.shulim@gmail.com>
2021-10-07 10:31:50 -0500

    errors

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -5,9 +5,9 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
+          <h3 className="loginLogo">Social VISA</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Connect with friends and the world around you on Social VISA.
           </span>
         </div>
         <div className="loginRight">
