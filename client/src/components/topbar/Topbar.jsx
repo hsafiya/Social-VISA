a1dfcbb48a60ca3bfe130f3a55f6943fb7d0fc93
Iryna <iryna.shulim@gmail.com>
2021-10-07 11:41:45 -0500

    bug fixes

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">Lamasocial</span>
+        <span className="logo">Social VISA</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -25,8 +25,8 @@ export default function Profile() {
                 />
               </div>
               <div className="profileInfo">
-                  <h4 className="profileInfoName">Safak Kocaoglu</h4>
-                  <span className="profileInfoDesc">Hello my friends!</span>
+                  <h4 className="profileInfoName">Alex Naumov</h4>
+                  <span className="profileInfoDesc">Hello there</span>
               </div>
             </div>
             <div className="profileRightBottom">
