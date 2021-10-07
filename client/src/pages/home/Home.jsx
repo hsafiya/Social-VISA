1c64a14255b3a08a32cac2fce15114393e26cb4b
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-10-07 07:50:16 -0500

    bug fixes Home page jsx file

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
-import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css";
 
 export default function Home() {
   return (
     <>
-      <Topbar />;
+      <Topbar />
       <div className="homeContainer">
         <Sidebar />
         <Feed />
