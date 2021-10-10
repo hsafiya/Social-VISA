8c8dbf7cf7279c53219f9b7c8463e8a2e8b1b2f1
Victor Lupascu <sinajeen@gmail.com>
2021-10-10 12:15:34 -0500

    edit

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,17 +1,17 @@
-import "./topbar.css";
-import { Search, Person, Chat, Notifications } from "@material-ui/icons";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
+import './topbar.css'
+import { Search, Person, Chat, Notifications } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
 
 export default function Topbar() {
-  const { user } = useContext(AuthContext);
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useContext(AuthContext)
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">Lamasocial</span>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <span className="logo">Social VISA</span>
         </Link>
       </div>
       <div className="topbarCenter">
@@ -47,7 +47,7 @@ export default function Topbar() {
             src={
               user.profilePicture
                 ? PF + user.profilePicture
-                : PF + "person/noAvatar.png"
+                : PF + 'person/noAvatar.png'
             }
             alt=""
             className="topbarImg"
@@ -55,5 +55,5 @@ export default function Topbar() {
         </Link>
       </div>
     </div>
-  );
+  )
 }
--- server/server.js
+++ server/server.js
@@ -1,63 +1,64 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const multer = require("multer");
-const userRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
-const conversationRoute = require("./routes/conversations");
-const messageRoute = require("./routes/messages");
-const router = express.Router();
-const path = require("path");
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const multer = require('multer')
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
+const conversationRoute = require('./routes/conversations')
+const messageRoute = require('./routes/messages')
+const router = express.Router()
+const path = require('path')
 
-dotenv.config();
-
+dotenv.config()
 
 // connect to DB
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/social-visa',
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   },
   () => {
     console.log('Connected to DB')
   },
 )
 
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
 
 //middleware
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'))
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images");
+    cb(null, 'public/images')
   },
   filename: (req, file, cb) => {
-    cb(null, req.body.name);
+    cb(null, req.body.name)
   },
-});
+})
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
+const upload = multer({ storage: storage })
+app.post('/api/upload', upload.single('file'), (req, res) => {
   try {
-    return res.status(200).json("File uploded successfully");
+    return res.status(200).json('File uploded successfully')
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-});
+})
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/conversations", conversationRoute);
-app.use("/api/messages", messageRoute);
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
+app.use('/api/conversations', conversationRoute)
+app.use('/api/messages', messageRoute)
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
-});
+  console.log('Backend server is running!')
+})
