e42937622b46403de2ccef2ea5cb76feda5847a9
Safiyya H <safiyya.hasanova@gmail.com>
2021-10-10 10:24:03 -0500

    init socket1

--- socket/index.js
+++ socket/index.js
@@ -3,3 +3,9 @@ const io = require("socket.io")(8900, {
       origin: "http://localhost:3000",
     },
   });
+  let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
