041cb03f95ab98fc0ceed61604097f8ee921fdca
Safiyya H <safiyya.hasanova@gmail.com>
2021-10-10 10:24:37 -0500

    remove user

--- socket/index.js
+++ socket/index.js
@@ -9,3 +9,11 @@ const addUser = (userId, socketId) => {
   !users.some((user) => user.userId === userId) &&
     users.push({ userId, socketId });
 };
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+  };
+  
+  const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+  
