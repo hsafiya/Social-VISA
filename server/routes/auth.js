a71b0167f3b660a4984e49e221124d7b69b58e97
Safiyya H <safiyya.hasanova@gmail.com>
2021-10-10 10:36:18 -0500

    convers routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,47 +1,42 @@
-const router = require('express').Router()
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-// SIGNUP
-router.post('/signup', async (req, res) => {
+//REGISTER
+router.post("/register", async (req, res) => {
   try {
-    //   hashing password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPsw = await bcrypt.hash(req.body.password, salt)
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    // create new user
+    //create new user
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hashedPsw,
-    })
+      password: hashedPassword,
+    });
 
-    // return new user
-    const user = await newUser.save()
-    res.status(200).json(user)
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err)
   }
-})
+});
 
-// LOGIN
-router.post('/login', async (req, res) => {
+//LOGIN
+router.post("/login", async (req, res) => {
   try {
-    //   find user by email
-    const user = await User.findOne({
-      email: req.body.email,
-    })
-    !user && res.status(404).json('user not found')
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
 
-    // compare the passwords
-    const validPsw = await bcrypt.compare(req.body.password, user.password)
-    !validPsw && res.status(400).json('wrong password')
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
 
-    // return user
     res.status(200).json(user)
   } catch (err) {
     res.status(500).json(err)
   }
-})
+});
 
-module.exports = router
+module.exports = router;
