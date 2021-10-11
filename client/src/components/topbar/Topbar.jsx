453e3bcae96b6707ff03c17d472e6cc97ad6d2cd
Victor Lupascu <sinajeen@gmail.com>
2021-10-11 01:08:50 -0500

    add account options dropdown menu

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,36 @@
 import './topbar.css'
-import { Search, Person, Chat, Notifications } from '@material-ui/icons'
+import {
+  Search,
+  Person,
+  Chat,
+  Notifications,
+  Settings,
+} from '@material-ui/icons'
 import { Link } from 'react-router-dom'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from '../../context/AuthContext'
+import Avatar from '@mui/material/Avatar'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import Logout from '@mui/icons-material/Logout'
 
 export default function Topbar() {
   const { user } = useContext(AuthContext)
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -29,30 +53,102 @@ export default function Topbar() {
           <span className="topbarLink">Timeline</span>
         </div>
         <div className="topbarIcons">
-          <div className="topbarIconItem">
-            <Person />
-            <span className="topbarIconBadge">1</span>
-          </div>
-          <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">2</span>
-          </div>
-          <div className="topbarIconItem">
-            <Notifications />
-            <span className="topbarIconBadge">1</span>
-          </div>
+          <Tooltip title="Friend Requests">
+            <div className="topbarIconItem">
+              <Person />
+              <span className="topbarIconBadge">1</span>
+            </div>
+          </Tooltip>
+          <Tooltip title="Messages">
+            <div className="topbarIconItem">
+              <Chat />
+              <span className="topbarIconBadge">2</span>
+            </div>
+          </Tooltip>
+
+          <Tooltip title="Notifications">
+            <div className="topbarIconItem">
+              <Notifications />
+              <span className="topbarIconBadge">1</span>
+            </div>
+          </Tooltip>
         </div>
-        <Link to={`/profile/${user.username}`}>
-          <img
-            src={
-              user.profilePicture
-                ? PF + user.profilePicture
-                : PF + 'person/noAvatar.png'
-            }
-            alt=""
-            className="topbarImg"
-          />
-        </Link>
+
+        <Tooltip title="Click for Options">
+          <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
+            <Avatar sx={{ width: 32, height: 32 }}>
+              <img
+                src={
+                  user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + 'person/noAvatar.png'
+                }
+                alt=""
+                // className="topbarImg"
+              />
+            </Avatar>
+          </IconButton>
+        </Tooltip>
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        >
+          <Link
+            to={`/profile/${user.username}`}
+            style={{ textDecoration: 'none', color: 'rgba(0, 0, 0, 0.87)' }}
+          >
+            <MenuItem>
+              <Avatar /> Profile
+            </MenuItem>
+          </Link>
+
+          <MenuItem>
+            <Avatar /> My account
+          </MenuItem>
+          <Divider />
+          <MenuItem>
+            <ListItemIcon>
+              <Settings fontSize="small" />
+            </ListItemIcon>
+            Settings
+          </MenuItem>
+          <MenuItem>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            <Link to={`/profile/${user.username}`}>Logout</Link>
+          </MenuItem>
+        </Menu>
       </div>
     </div>
   )
